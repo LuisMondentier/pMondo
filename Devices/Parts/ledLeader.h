aa6ba303c48cdafce9764934ae02e3a92d71658f
LuisMondentier <luis.fernangonz@gmail.com>
2015-03-14 15:56:09 +0100

    Created ledLeader library TODOs: create ledLeader cpp based on AVR microcontroller tutorial

---
+++ Devices/Parts/ledLeader.h
@@ -0,0 +1,21 @@
+/////////////////////////////////////////
+//
+//	Project_Mondo
+//
+//	Author: Luis Fernández González
+//
+/////////////////////////////////////////
+//	ledLeader.h
+
+#ifndef LED_LEADER_H_
+#define LED_LEADER_H_
+
+class LedLeader {
+public:
+	void turn_on();
+	void turn_off();
+private:
+	int _id;
+}
+
+#endif	//LED_LEADER_H_
